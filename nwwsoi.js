1d01e26d55cfdd47e36de4504bf33c6a055f8c0d
tgranz <nimbusapps@proton.me>
2026-03-05 17:31:10 -0500

    Bugfixes and add params for SSEs

--- nwwsoi.js
+++ nwwsoi.js
@@ -188,7 +188,7 @@ export default class NWWSOI {
                         console.log(`Attempting to ${action} alert with eventTrackingNumber: ${eventTrackingNumber}`);
                         try {
                             updateAlert(alertIdentity, alertData);
-                            callbacks.onUpdate();
+                            callbacks.onUpdate(alertData);
                             return;
                         } catch (err) {
                             console.warn('Failed to update alert:', err.message, '- Adding as new alert instead');
@@ -200,7 +200,7 @@ export default class NWWSOI {
                 // Push this alert to the database
                 try {
                     addNewAlert(alertData);
-                    callbacks.onNew();
+                    callbacks.onNew(alertData);
                     console.log('Successfully stored alert in database\n');
                 } catch (err) {
                     console.error('Error saving alert to database:', err.message);
